190d33acec86edbe59f8b88d5a9f96fce94cd9f1
lieby weintraub <liebahinda@gmail.com>
2019-08-05 01:45:31 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'products#index'
+  #the root route 
   post '/', to: 'products#add'
 end
